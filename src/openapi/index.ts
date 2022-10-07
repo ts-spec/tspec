f3f2bb95ef4f82ef5eeae3d73724404d163daeaa
Hyeonss <hyeonss0417@gmail.com>
2022-10-07 15:07:14 +0900

    feat(generateOAS): add components property and add overriding spec

--- src/openapi/index.ts
+++ src/openapi/index.ts
@@ -2,21 +2,35 @@ import { OpenAPIV3 } from 'openapi-types';
 import { Tspec } from "../types/tspec";
 import { getOpenApiType } from './getOpenApiType';
 
-export const generateOpenApiSpec = (specs: Tspec.ParsedApiSpec[]): OpenAPIV3.Document => ({
+export const generateOpenApiSpec = (specs: Tspec.ParsedApiSpec[], override?: Partial<OpenAPIV3.Document>): OpenAPIV3.Document => ({
     openapi: '3.0.0',
     info: {
       title: 'API',
       version: '1.0.0',
+      ...override?.info,
     },
-    servers: [{ url: '/' }],
-    tags: buildTags(specs),
+    servers: override?.servers || [{ url: '/' }],
+    tags: buildTags(specs, override?.tags || []),
     paths: buildPaths(specs),
+    components: buildComponents(specs, override?.components || {}),
 });
 
-const buildTags = (specs: Tspec.ParsedApiSpec[]): OpenAPIV3.TagObject[] => {
-  const tags = specs.map((spec) => spec.tags || []).flat();
-  const dedupedTags = [...new Set(tags)];
-  return dedupedTags.map((tag) => ({ name: tag }));
+const buildTags = (specs: Tspec.ParsedApiSpec[], tags: OpenAPIV3.TagObject[]): OpenAPIV3.TagObject[] => {
+  const tagMap: Record<string, OpenAPIV3.TagObject> = {};
+
+  tags?.forEach((tag) => {
+    tagMap[tag.name] = tag;
+  });
+
+  specs.forEach((spec) => {
+    spec.tags?.forEach((tag) => {
+      if (!tagMap[tag]) {
+        tagMap[tag] = { name: tag };
+      }
+    });
+  });
+  
+  return Object.values(tagMap);
 }
 
 const buildPaths = (specs: Tspec.ParsedApiSpec[]): OpenAPIV3.PathsObject => {
@@ -31,6 +45,13 @@ const buildPaths = (specs: Tspec.ParsedApiSpec[]): OpenAPIV3.PathsObject => {
   }
 
   return paths;
+}
+
+const buildComponents = (specs: Tspec.ParsedApiSpec[], components: OpenAPIV3.ComponentsObject): OpenAPIV3.ComponentsObject => {
+  return {
+    // TODO: define schemas
+    securitySchemes: components.securitySchemes,
+  };
 }
 
 const buildOperation = (spec: Tspec.ParsedApiSpec): OpenAPIV3.OperationObject => {
