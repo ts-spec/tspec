2780d9de773c5f3929a18ec2d1be6e88ac998382
Hyeonss <hyeonss0417@gmail.com>
2023-06-01 18:02:32 +0900

    docs: API Testing

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
           { text: 'Defining API Spec', link: '/guide/defining-api-spec' },
           { text: 'CRUD API Example', link: '/guide/crud-api-example' },
           { text: 'Express Integration', link: '/guide/express-integration' }, // with Request validation
-          // { text: 'API Testing', link: '/guide/api-testing' },
+          { text: 'API Testing', link: '/guide/api-testing' },
           // { text: 'Authentication', link: '/guide/authentication' },
         ]
       },
